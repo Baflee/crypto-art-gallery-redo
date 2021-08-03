a9dccf631eceec2a46effbd5bdd8e0f77fc26624
Gerard <gerard@holovr-prod.com>
2021-08-03 11:05:45 +0200

    ajout d'une lumiere supplementaire qui change selon les heures

--- Assets/Scripts/Customs/SkyboxHours.cs
+++ Assets/Scripts/Customs/SkyboxHours.cs
@@ -8,6 +8,7 @@ namespace tiltbrush
     {
         public Material SkyboxPano;
         public GameObject Sun;
+        public Light Moon;
         public TiltBrush.SceneSettings SceneSettings;
         [Range(0f, 86400f)]
         [SerializeField] public float Jour;
@@ -15,10 +16,12 @@ namespace tiltbrush
         GradientColorKey[] PaletteJourCouleur;
         GradientAlphaKey[] PaletteJourTransparent;
         float H_Minuit = 86400f;
+        float H_Soir = 64800f;
         float H_Midi = 43200f;
+        float H_Mat = 28800f;
         float H_Matin = 0f;
-        float h_MilieuMatin;
-        float h_MilieuApresMidi;
+        float H_MilieuMatin;
+        float H_MilieuApresMidi;
         public Color Matinee;
         public Color Midi;
         public Color Soiree;
@@ -33,8 +36,8 @@ namespace tiltbrush
             Midi = Color.HSVToRGB(colorvalue + 0.072f, 0.6f, 1f);
             Soiree = Color.HSVToRGB(colorvalue + 0.144f, 1f, 0.25f);
 
-            h_MilieuMatin = Random.Range(28800f, 39600f);
-            h_MilieuApresMidi = Random.Range(46800f, 61200f);
+            H_MilieuMatin = Random.Range(28800f, 39600f);
+            H_MilieuApresMidi = Random.Range(46800f, 61200f);
 
             PaletteJourCouleur = new GradientColorKey[3];
             PaletteJourCouleur[0].color = Matinee;
@@ -83,20 +86,23 @@ namespace tiltbrush
                 DynamicGI.UpdateEnvironment();
             }
 
-            if (Jour == H_Matin)
+            if (Jour >= H_Matin && Jour <= H_MilieuMatin)
             {
-                RenderSettings.fogDensity = 1;
+                RenderSettings.fogDensity = (1f - (Jour/ H_MilieuMatin))/33f;
+            }
+            else if (Jour >= H_MilieuApresMidi && Jour <= H_Minuit)
+            {
+                RenderSettings.fogDensity = ((Jour - H_MilieuApresMidi) / (H_Minuit - H_MilieuApresMidi))/33f;
             }
 
-            if (Jour >= H_Matin && Jour <= h_MilieuMatin)
+            if (Jour >= H_Mat && Jour <= H_Midi)
             {
-                RenderSettings.fogDensity = (1f - (Jour/ h_MilieuMatin))/20f;
+                Moon.intensity = 0.5f + ((Jour - H_Mat) / (H_Midi - H_Mat));
             }
-            else if (Jour >= h_MilieuApresMidi && Jour <= H_Minuit)
+            else if (Jour >= H_Midi && Jour <= H_Soir)
             {
-                RenderSettings.fogDensity = ((Jour - h_MilieuApresMidi) / (H_Minuit - h_MilieuApresMidi))/20f;
+               Moon.intensity = 0.5f + (1f - (Jour / H_Soir));
             }
-
         }
 
     }
